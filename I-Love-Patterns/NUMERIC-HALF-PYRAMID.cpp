05d03eabf2b78891f4bd189bf8234114fece6c22
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2023-06-28 13:45:12 +0530

    Day 1 of solving patterns

---
+++ I-Love-Patterns/NUMERIC-HALF-PYRAMID.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int rowCount;
+    cout<<"Give Row Count Plz" <<endl;
+    cin>>rowCount;
+    for (int row = 0; row < rowCount; row = row+1)
+    {
+        for (int col = 0; col < row + 1; col = col+1)
+        {
+            cout<<col+1;
+        }
+        cout<<endl;
+    }
+    
+}
