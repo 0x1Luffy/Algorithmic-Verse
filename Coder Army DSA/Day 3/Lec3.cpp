b7f67ca096af5294d592c6820774afc8c48434d1
0x1Luffy <chetankesare06@gmail.com>
2024-04-22 00:21:19 +0530

    New Journey Started

---
+++ Coder Army DSA/Day 3/Lec3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+       cout<<"Hello Code Army"<<endl;
+       cout<<"Hello Code Army"<<endl;
+
+       cout<<3*9<<endl;
+       cout<<"***************************************************"<<endl;
+
+
+
+
+       // DATA TYPES 
+
+
+       //INT
+        int a=10, b=20, c=25;
+        cout<<a<<endl;
+        cout<<a+b+c<<endl;
+
+
+       //char
+       char C='B';
+       cout<<C<<endl;
+};
